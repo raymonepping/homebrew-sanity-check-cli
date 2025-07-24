08dabaa3c922d55ba5f023b996c258786b7bd215
Raymon Epping <your@email.com>
2025-07-24 12:08:15 +0200

    🔖 sanity-check-cli: release v1.1.7

--- Formula/sanity-check-cli.rb
+++ Formula/sanity-check-cli.rb
@@ -1,10 +1,10 @@
 class SanityCheckCli < Formula
   desc "Lint, format, and sanity-check your scripts, configs, and Docker/Terraform files"
   homepage "https://github.com/raymonepping/sanity_check_cli"
-  url "https://github.com/raymonepping/homebrew-sanity-check-cli/archive/refs/tags/v1.1.6.tar.gz"
-  sha256 "35f744d373c4d30d2ec1c7ca7f1652cf446d7af85df013dab6d6fa376625afea"
+  url "https://github.com/raymonepping/homebrew-sanity-check-cli/archive/refs/tags/v1.1.7.tar.gz"
+  sha256 "3959c8005008e3fc12f6f2dc4170e7b1615ca43ef5b42775f961fe786ddc5e68"
   license "MIT"
-  version "1.1.6"
+  version "1.1.7"
 
   depends_on "bash"
   depends_on "jq"
